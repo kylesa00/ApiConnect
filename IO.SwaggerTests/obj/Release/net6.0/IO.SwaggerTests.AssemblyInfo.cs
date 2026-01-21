6ba3d2ae2dd4432cd3e108886315515ea2756a89
uros <uros.negovanovic.lge@gmail.com>
2026-01-21 14:58:44 +0100

    commit

--- IO.SwaggerTests/obj/Release/net6.0/IO.SwaggerTests.AssemblyInfo.cs
+++ IO.SwaggerTests/obj/Release/net6.0/IO.SwaggerTests.AssemblyInfo.cs
@@ -12,7 +12,7 @@ using System;
 using System.Reflection;
 
 [assembly: System.Reflection.AssemblyCompanyAttribute("IO.SwaggerTests")]
-[assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
+[assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9e0a2ab444027464d36358f78ce0111fdc4e81f7")]
 [assembly: System.Reflection.AssemblyProductAttribute("IO.SwaggerTests")]
